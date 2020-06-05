cad3f4ea8d41f3fac20f549dbe39b9e5b8259d68
Giovana Daniele <48071009+giovanadanieles@users.noreply.github.com>
2020-06-04 22:55:53 -0300

    Mudando o nome do questionário Columbia

--- script.sql
+++ script.sql
@@ -54,7 +54,7 @@ CREATE TABLE instituto(
 
 INSERT INTO questionario VALUES ('SRQ-20', 'Nas próximas próximas perguntas, você realizará uma autoavaliação a respeito da sua saúde em geral. Serão 24 afirmativas, e você deve selecionar sim ou não para cada afirmativa. Não existe resposta certa ou errada, por isso pedimos que você responda da forma que você julgar melhor. Caso tenha alguma dúvida, você pode nos mandar uma mensagem pelo XXX.');
 								  
-INSERT INTO questionario VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Nessa escala, será avaliado o risco de suicídio através de algumas perguntas. Essa escala foi criada por pesquisadores de diversas universidades (Universidade de Columbia; Universidade da Pensilvânia; Universidade de Pittsburgh e Universidade de Nova Iorque) e avalia comportamentos específicos que podem ser indicativos da intenções de uma pessoa em cometer suicídio. É importante ressaltar que os resultados apresentados são referentes a um período específico de sua vida, e que também podem mudar de acordo com as condições ambientais e temporais. É importante destacar que este resultado não visa definir você enquanto “suicida”, mas, sim, mostrar a necessidade de você procurar ajuda para enfrentar as dificuldades que está passando, no momento. Por isso pedimos que você seja o mais sincero/a possível nas respostas, para que você saiba se é importante buscar ajuda, ou de familiares e amigos, ou de uma equipe de saúde especializada.');
+INSERT INTO questionario VALUES ('Columbia', 'Nessa escala, será avaliado o risco de suicídio através de algumas perguntas. Essa escala foi criada por pesquisadores de diversas universidades (Universidade de Columbia; Universidade da Pensilvânia; Universidade de Pittsburgh e Universidade de Nova Iorque) e avalia comportamentos específicos que podem ser indicativos da intenções de uma pessoa em cometer suicídio. É importante ressaltar que os resultados apresentados são referentes a um período específico de sua vida, e que também podem mudar de acordo com as condições ambientais e temporais. É importante destacar que este resultado não visa definir você enquanto “suicida”, mas, sim, mostrar a necessidade de você procurar ajuda para enfrentar as dificuldades que está passando, no momento. Por isso pedimos que você seja o mais sincero/a possível nas respostas, para que você saiba se é importante buscar ajuda, ou de familiares e amigos, ou de uma equipe de saúde especializada.');
 
 
 
@@ -122,22 +122,22 @@ INSERT INTO pergunta (questOrigPerg, conteudoPerg)
 		
 		
 INSERT INTO pergunta (questOrigPerg, conteudoPerg)
-			VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Você desejou estar morto(a) ou desejou poder dormir e nunca mais acordar?');
+			VALUES ('Columbia', 'Você desejou estar morto(a) ou desejou poder dormir e nunca mais acordar?');
 					
 INSERT INTO pergunta (questOrigPerg, conteudoPerg)
-			VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Você já pensou realmente em se matar?');
+			VALUES ('Columbia', 'Você já pensou realmente em se matar?');
 					 
 INSERT INTO pergunta (questOrigPerg, conteudoPerg)
-			VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Você tem pensado em como poderia fazer isso?');
+			VALUES ('Columbia', 'Você tem pensado em como poderia fazer isso?');
 					
 INSERT INTO pergunta (questOrigPerg, conteudoPerg)
-			VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Você teve esses pensamentos e teve alguma intenção de colocá-los em prática?');
+			VALUES ('Columbia', 'Você teve esses pensamentos e teve alguma intenção de colocá-los em prática?');
 					
 INSERT INTO pergunta (questOrigPerg, conteudoPerg)
-			VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Você já começou a elaborar ou já elaborou os detalhes de como se matar? Você pretende executar esse plano?');
+			VALUES ('Columbia', 'Você já começou a elaborar ou já elaborou os detalhes de como se matar? Você pretende executar esse plano?');
 					
 INSERT INTO pergunta (questOrigPerg, conteudoPerg)
-			VALUES ('Avaliação do Risco de Suicídio de Columbia', 'Você já fez alguma coisa, começou a fazer algo ou planejou fazer alguma coisa para acabar com sua vida?');	
+			VALUES ('Columbia', 'Você já fez alguma coisa, começou a fazer algo ou planejou fazer alguma coisa para acabar com sua vida?');	
 					
 					
 					
